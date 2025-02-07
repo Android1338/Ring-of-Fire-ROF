18f248c0c15616460505269a5f5577cfc564b03c
Julian Merker <info@julian-merker.at>
2025-02-07 12:53:58 +0100

    Changes until video 14

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,19 +1,56 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
   pickCardAnimation:boolean = false;
+  game: Game = new Game();
+  currentCard: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game)
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      if (this.game.stack.length > 0) {
+        const drawnCard = this.game.stack.pop();
+  
+        if (drawnCard !== undefined) {
+          this.currentCard = drawnCard;
+          this.pickCardAnimation = true;
+          console.log('Current card:',this.currentCard);
+          console.log('Game:',this.game);
+  
+          setTimeout(() => {
+            this.pickCardAnimation = false;
+            this.game.playedCards.push(this.currentCard);
+          }, 1000);
+        }
+      }
+      else {
+        console.log("Spiel beendet!");
+      }
+    }
+  }
+
+  newGame() {
+    this.game;
   }
 
 }

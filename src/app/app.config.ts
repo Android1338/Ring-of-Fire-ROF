bf5b90c5d2b6146d9ac48c03bf46166ea92a89f0
Julian Merker <info@julian-merker.at>
2025-03-05 15:37:58 +0100

    Starting implementing Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-e746c","appId":"1:894265477103:web:33bf0db0da13a9de9bd81a","storageBucket":"ring-of-fire-e746c.firebasestorage.app","apiKey":"AIzaSyCEycyTUrvOzkwD3soqgbIFu0Qrb1RQeEU","authDomain":"ring-of-fire-e746c.firebaseapp.com","messagingSenderId":"894265477103"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from "../player/player.component";
 import { MatIconModule } from '@angular/material/icon';
@@ -12,25 +12,43 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { GameInfoComponent } from "../game-info/game-info.component"; 
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatDividerModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatDialogModule, GameInfoComponent],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatDividerModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatDialogModule, GameInfoComponent, AsyncPipe],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit {
-
+  items$: Observable<any[]>; // Typisiere hier entsprechend deiner Bedürfnisse
   pickCardAnimation:boolean = false;
   game: Game = new Game();
+  itemsCollection:any;
   currentCard: string = '';
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public firestore: Firestore, public dialog: MatDialog) {
+    const itemsCollection = collection(this.firestore, 'games');
+    this.items$ = collectionData(itemsCollection);
+  }
 
   ngOnInit(): void {
     this.newGame();
     this.openDialog();
+
+    this.itemsCollection = collection(this.firestore, 'games'); // Bestimme die Collection hier
+    this.items$ = collectionData(this.itemsCollection); // Verknüpfe die Observable mit der Collection
+
+    this.items$.subscribe(data => {
+      console.log('Game update', data)
+    });
+  }
+
+  newGame() {
+    this.game;
   }
 
 
@@ -60,10 +78,6 @@ export class GameComponent implements OnInit {
         console.log("Spiel beendet!");
       }
     }
-  }
-
-  newGame() {
-    this.game;
   }
 
   openDialog(): void {
